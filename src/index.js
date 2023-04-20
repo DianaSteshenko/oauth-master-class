0f6fde5a79add567d2b733198c335520541aa2b0
DianaSteshenko <39907111+DianaSteshenko@users.noreply.github.com>
2023-04-20 12:10:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,22 @@ const fetchYandexData = (token) =>
 
 window.onload = () => {
   document.getElementById("suggest").onclick = () => {
-    // TODO suggest
+YaAuthSuggest.init({
+         client_id: 'f3fa733aa242417ca72760aaef5a3c82', 
+         response_type: 'token',
+         redirect_uri: 'https://oauth-master-class-icaw.vercel.app/token.html'
+      },
+      'https://oauth-master-class-icaw.vercel.app/'
+   )
+  .then(({ handler }) => handler())
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+
+        authorize(result);
+
+        console.log(result, data);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
   };
   document.getElementById("button").onclick = () => {
     // TODO button
